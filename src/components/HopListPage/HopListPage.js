54f2aa55e2c70879878b9cd6fb6156fa13414d08
mes32 <stockman.mike@gmail.com>
2019-02-11 14:48:45 -0600

    Add some CSS style to table on HopListPage.

--- src/components/HopListPage/HopListPage.js
+++ src/components/HopListPage/HopListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import './HopListPage.css';
 import HopListRow from './HopListRow/HopListRow';
 
 class HopListPage extends Component {
