be130d54e1a6d88b0e544e5c8e434b4daf1bbe39
mes32 <stockman.mike@gmail.com>
2019-02-13 16:57:05 -0600

    Get timing working with componentDidUpdate() on HopVarietyPage.

--- src/components/HopVarietyPage/HopVarietyPage.js
+++ src/components/HopVarietyPage/HopVarietyPage.js
@@ -1,33 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const DEFAULT_HOP = {
+    variety_name: '[ Hop Variety Name ]',
+    country_id: null,
+    aromas: null,
+    brewing_role_id: null,
+    alpha_acid_min: null,
+    alpha_acid_max: null,
+    beta_acid_min: null,
+    beta_acid_max: null,
+    cohumulone_min: null,
+    cohumulone_max: null,
+    total_oil_min: null,
+    total_oil_max: null,
+    beta_pinene_min: null,
+    beta_pinene_max: null,
+    myrcene_min: null,
+    myrcene_max: null,
+    linalool_min: null,
+    linalool_max: null,
+    caryophyllene_min: null,
+    caryophyllene_max: null,
+    farnesene_min: null,
+    farnesene_max: null,
+    humulene_min: null,
+    humulene_max: null,
+    geraniol_min: null,
+    geraniol_max: null,
+    selinene_min: null,
+    selinene_max: null,
+    other_oils_min: null,
+    other_oils_max: null
+}
+
 class HopVarietyPage extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            hop: {
-                variety_name: '[ Hop Variety Name ]',
-                country: '[ Country of Origin ]',
-                aromas: '[ aromas ]'
-            }
+            hop: DEFAULT_HOP
         };
     }
 
     componentDidMount() {
-        const action = { type: 'FETCH_HOPS' };
-        this.props.dispatch(action);
         const id = this.props.match.params.id;
-        console.log('id:', id);
-        const hops = this.props.reduxStore.hops;
-        console.log('hop:', hops);
-        // this.setState({
-        //     hop: hop
-        // });
+        const action = { type: 'FETCH_FOCUS_HOP', payload: id };
+        this.props.dispatch(action);
     }
 
-    // Needs some life-cycle function love
-    // propsDidChange()
+    componentDidUpdate(prevProps, prevState) {
+        const prevHop = prevProps.reduxStore.focusHop;
+        const hop = this.props.reduxStore.focusHop;
+        console.log('hop:', hop.variety_name);
+        if (hop !== prevHop) {
+            this.setState({
+                hop: hop}
+            );
+        }
+    }
 
     render() {
         return (
