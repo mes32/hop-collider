b8c36db9f28c8c2be35a801594d29ccf3061a3f9
mes32 <stockman.mike@gmail.com>
2019-02-13 23:14:46 -0600

    Start building HopComparisonPage with a single drop-down menu.

--- src/components/HopComparisonPage/HopComparisonPage.js
+++ src/components/HopComparisonPage/HopComparisonPage.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class HopComparisonPage extends Component {
+    componentDidMount() {
+        const action = { type: 'FETCH_HOPS' };
+        this.props.dispatch(action);
+    }
+
+    selectHop = (event) => {
+        const id = event.target.value;
+        if (id === '') {
+            // clear plots
+        } else {
+            const action = { type: 'FETCH_FOCUS_HOP', payload: id };
+            this.props.dispatch(action);
+        }
+    }
+
     render() {
         return (
             <div>
-                <p>[ HopComparisonPage ]</p>
+                <h2>Hop Comparison</h2>
+                <select onChange={this.selectHop} defaultValue="">
+                    <option value="">-- Select a Hop --</option>
+                    {this.props.reduxStore.hops.map(
+                        (hop) => <option key={hop.id} value={hop.id}>{hop.variety_name} ({hop.country})</option>
+                    )}
+                </select>
             </div>
         );
     }
 }
 
-export default HopComparisonPage;
+const mapReduxStoreToProps = (reduxStore) => ({ reduxStore });
+export default connect(mapReduxStoreToProps)(HopComparisonPage);
